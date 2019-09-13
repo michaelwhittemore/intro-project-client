e69df632a8a53f3cf773f7a8e3d9c5b710ae54b6
Michael Whittemore <michael.whittemore@vonage.com>
2019-09-13 11:12:25 -0700

    disconnecting

--- js/app.js
+++ js/app.js
@@ -6,8 +6,8 @@ function handleError(error) {
     }
 }
 
-var SERVER_BASE_URL = 'https://intro-project.herokuapp.com';
-//var SERVER_BASE_URL="localhost:3000" //Remove this later, use for testing
+//var SERVER_BASE_URL = 'https://intro-project.herokuapp.com';
+var SERVER_BASE_URL = "http://localhost:3000" //Remove this later, use for testing
 window.onload = () => {
     //check if we already have a userId, if not ping the server to set up a user
     if (sessionStorage.getItem('userId') === null) {
@@ -25,28 +25,31 @@ window.onload = () => {
     }
     //enter the video queue
     document.getElementById("start-video").onclick = () => {
-        console.log('look for vid')
-        //TODO make call to sever adding to the queue and starting a session
+        console.log('looking for vid')
+        // make call to sever adding to the queue and starting a session
         addToVideoQueue(sessionStorage.getItem('userId'), sessionStorage.getItem('userRole'))
     }
+    
 
 }
 function addToVideoQueue(Id, role) {
-    console.log('sending ' + JSON.stringify({ userId: Id, userRole: role }))
-    fetch(SERVER_BASE_URL + '/queue', {
-        mode:'cors',
-        method: 'POST',
-        headers: {
-            'Access-Control-Allow-Origin': '*'
-        },
-        body: JSON.stringify({ userId: Id, userRole: role })
-    }).then(res => {
-        apiKey = res.apiKey;
-        console.log(res)
-        sessionId = res.sessionId;
-        token = res.token;
-        initializeSession();
-    }).catch(handleError);
+    let body = JSON.stringify({ userId: Id, userRole: role })
+    console.log('sending ' + body)
+    fetch(SERVER_BASE_URL + '/queue',
+        {
+            method: 'POST',
+            body: body,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(async (res) => {
+            let resJson = await res.json()
+            apiKey = resJson.apiKey;
+            console.log(resJson)
+            sessionId = resJson.sessionId;
+            token = resJson.token;
+            initializeSession(apiKey, sessionId);
+        }).catch(handleError);
 }
 
 //sets up a newuser from session storage
@@ -54,6 +57,63 @@ function setUser() {
     document.getElementById("start-video").disabled = false;
     document.getElementById("connected").innerText = `User Id (for troubleshooting): ${sessionStorage.getItem('userId')} \nUser Role: ${sessionStorage.getItem('userRole')}`
 }
+//updates the buttons, should be called whenever we connect to a new session
+function newSessionButtons(session) {
+    document.getElementById("start-video").disabled = true;
+    document.getElementById("disconnect-video").disabled = false;
+    // TODO first clear all existing listners of of the button
+    document.getElementById("disconnect-video").onclick = () => {
+        session.disconnect();
+    }
+}
+function noSessionButtons(){
+    console.log('no session called')
+    document.getElementById('start-video').disabled = false;
+    document.getElementById('disconnect-video').disabled = true;
+}
+
+function initializeSession(apiKey, sessionId) {
+    console.log(apiKey, sessionId)
+    var session = OT.initSession(apiKey, sessionId);
+
+    // Subscribe to a newly created stream
+    session.on('streamCreated', function (event) {
+        session.subscribe(event.stream, 'subscriber', {
+            insertMode: 'append',
+            width: '100%',
+            height: '100%'
+        }, handleError);
+    });
+
+    // Create a publisher
+    var publisher = OT.initPublisher('publisher', {
+        insertMode: 'append',
+        width: '100%',
+        height: '100%'
+    }, handleError);
+
+    // Connect to the session
+    session.connect(token, function (error) {
+        // If the connection is successful, initialize a publisher and publish to the session
+        if (error) {
+            handleError(error);
+        } else {
+            session.publish(publisher, handleError);
+            newSessionButtons(session);
+        }
+    });
+
+    session.on('connectionDestroyed', function (event) {
+        console.log('connection destroyed. logging event: ', event)
+        noSessionButtons();
+    })
+    session.on('sessionDisconnected', function (event) {
+        console.log('session disconnected. logging event: ', event)
+        noSessionButtons();
+    })
+}
+
+
 
 
 //start a video feed
@@ -66,35 +126,3 @@ function setUser() {
 //     token = res.token;
 //     initializeSession();
 // }).catch(handleError);
-
-
-
-function initializeSession() {
-    var session = OT.initSession(apiKey, sessionId);
-
-    // Subscribe to a newly created stream
-    session.on('streamCreated', function (event) {
-        session.subscribe(event.stream, 'subscriber', {
-            insertMode: 'append',
-            wIdth: '100%',
-            height: '100%'
-        }, handleError);
-    });
-
-    // Create a publisher
-    var publisher = OT.initPublisher('publisher', {
-        insertMode: 'append',
-        wIdth: '100%',
-        height: '100%'
-    }, handleError);
-
-    // Connect to the session
-    session.connect(token, function (error) {
-        // If the connection is successful, initialize a publisher and publish to the session
-        if (error) {
-            handleError(error);
-        } else {
-            session.publish(publisher, handleError);
-        }
-    });
-}
